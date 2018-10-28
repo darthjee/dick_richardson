8ac3776ae34f0835dd6ac55a9cf91d1030843e32
darthjee <darthjee@gmail.com>
2018-10-28 17:11:26 +0100

    Add Ballot#create_partial

--- spec/factories/ballot.rb
+++ spec/factories/ballot.rb
@@ -2,6 +2,12 @@ FactoryBot.define do
   factory :simulation_ballot, class: Simulation::Ballot do
     simulation
     percentage 0.7
+
+    trait :with_partial do
+      after :create do |ballot, _|
+        ballot.create_partial
+      end
+    end
   end
 end
 
