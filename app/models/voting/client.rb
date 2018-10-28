8b18b7d74b61d2729ccb599fede0b6d7a86930ea
darthjee <darthjee@gmail.com>
2018-10-28 20:47:47 +0100

    Add voting client

---
+++ app/models/voting/client.rb
@@ -0,0 +1,23 @@
+module Voting
+  class Client
+    def get
+      JSON.parse(connection.get.body)
+    end
+
+    def connection
+      @connection ||= build_connection
+    end
+
+    def build_connection
+      Faraday.new(url: url) do |faraday|
+        faraday.request  :url_encoded
+        faraday.response :logger
+        faraday.adapter  Faraday.default_adapter
+      end
+    end
+
+    def url
+      "http://divulga.tse.jus.br/2018/divulgacao/oficial/296/dadosdivweb/br/br-c0001-e000296-w.js?#{Time.now.to_i}"
+    end
+  end
+end
