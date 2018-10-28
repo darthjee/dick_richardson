4a61979c93dc71deb786c87282dda5af1c857387
darthjee <darthjee@gmail.com>
2018-10-28 22:04:06 +0100

    Add candidates to partial

--- app/models/voting/partial.rb
+++ app/models/voting/partial.rb
@@ -2,5 +2,6 @@ module Voting
   class Partial < ActiveRecord::Base
     belongs_to :voting
     has_many :partial_candidates
+    has_many :candidates, class_name: 'Voting::PartialCandidate'
   end
 end
