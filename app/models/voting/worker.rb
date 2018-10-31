4a733d722431c4cc45c2805009882eba872e1f28
darthjee <darthjee@gmail.com>
2018-10-31 09:56:54 +0100

    Processor process raw data

--- app/models/voting/worker.rb
+++ app/models/voting/worker.rb
@@ -1,47 +1,23 @@
 module Voting
   class Worker
-    delegate :votes, :candidates, :raw, to: :parser
-
-    delegate :partials, to: :voting
-
-    attr_reader :voting, :partial
+    attr_reader :voting
 
     def initialize(voting  = ::Voting::Voting.first)
       @voting = voting
     end
 
     def perform
-      return unless voting.active
-      return unless new_entry?
-      create_partial
-      create_partial_candidates
+      processor.process
     end
 
     private
-
-    def create_partial
-      @partial = partials.create(votes: votes, raw: raw)
-    end
-
-    def create_partial_candidates
-      candidates.each do |candidate|
-        voting_candidate = voting.candidates.find_or_create_by(name: candidate.name)
-
-        partial.partial_candidates.create(candidate: voting_candidate, votes: candidate.votes)
-      end
-    end
-
-    def new_entry?
-      return true if partials.empty?
-      votes > partials.maximum(:votes)
-    end
 
     def client
       @client ||= Client.new
     end
 
-    def parser
-      @parser ||= Parser.new(raw: client.get)
+    def processor
+      @processor ||= Processor.new(voting, raw: client.get)
     end
   end
 end
--- spec/models/voting/processor_spec.rb
+++ spec/models/voting/processor_spec.rb
@@ -1,22 +1,12 @@
 require 'spec_helper'
 
 describe Voting::Processor do
-  subject(:processor) { described_class.new(voting) }
+  subject(:processor) { described_class.new(voting, raw: raw) }
 
   let(:voting) { create(:voting) }
 
   describe '#process' do
-    let(:request_result) { load_fixture_file('voting/tse_response_partial.json') }
-    let(:time_integer)   { Time.now.to_i }
-
-    let(:request_url) do
-      "http://divulga.tse.jus.br/2018/divulgacao/oficial/296/dadosdivweb/br/br-c0001-e000296-w.js?#{time_integer}"
-    end
-
-    before do
-      allow_any_instance_of(Time).to receive(:to_i).and_return(time_integer)
-      stub_request(:get, request_url).to_return(status: 200, body: request_result)
-    end
+    let(:raw) { load_fixture_file('voting/tse_response_partial.json') }
 
     context 'when there is no partial' do
       it 'creates the partial' do
@@ -44,7 +34,7 @@ describe Voting::Processor do
 
       it 'records the raw result' do
         processor.process
-        expect(voting.partials.last.raw).to eq(request_result)
+        expect(voting.partials.last.raw).to eq(raw)
       end
 
       it 'sets the full votes' do
