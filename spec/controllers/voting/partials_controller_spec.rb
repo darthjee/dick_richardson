f8c7b64ecab3c582241c33b413b34b64772db9b3
darthjee <darthjee@gmail.com>
2018-11-01 17:48:22 +0100

    Add specs for controller

--- spec/controllers/voting/partials_controller_spec.rb
+++ spec/controllers/voting/partials_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe Voting::PartialsController, type: :controller do
-  let(:voting)  { create(:voting) }
+  let(:voting)  { create(:voting, active: active) }
+  let(:active)  { true }
   let(:payload) { load_json_fixture_file('voting/tse_response_partial.json') }
 
   let(:parameters) do
@@ -13,10 +14,51 @@ describe Voting::PartialsController, type: :controller do
 
   describe 'POST create' do
     context 'when the request happens' do
-      it 'creates a new partial' do
-        expect do
-          post :create, params: parameters
-        end.to change { voting.partials.count }.by(1)
+      context 'when it is a new partial result' do
+        it 'creates a new partial' do
+          expect do
+            post :create, params: parameters
+          end.to change { voting.partials.count }.by(1)
+        end
+      end
+
+      context 'when there is a registered partial' do
+        include_context 'voting with one partial'
+
+        before do
+          voting.candidates.each do |candidate|
+            partial.candidates.create(candidate: candidate, votes: 50)
+          end
+        end
+
+        context 'for the same result as the new partial' do
+          let(:partial_raw)   { load_fixture_file('voting/tse_response_partial.json') }
+          let(:current_votes) { 147303938 }
+
+          it 'does not create a new partial' do
+            expect do
+              post :create, params: parameters
+            end.not_to change { voting.partials.count }
+          end
+        end
+
+        context 'for an older result' do
+          it 'creates a new partial' do
+            expect do
+              post :create, params: parameters
+            end.to change { voting.partials.count }.by(1)
+          end
+        end
+      end
+
+      context "when voting is inactive" do
+        let(:active) { false }
+
+        it 'does not create a new partial' do
+          expect do
+            post :create, params: parameters
+          end.not_to change { voting.partials.count }
+        end
       end
     end
 
