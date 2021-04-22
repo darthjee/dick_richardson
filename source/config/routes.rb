586f2ebc2578a0f3065abf28d300cc7d3d857fbc
darthjee <darthjee@gmail.com>
2021-04-23 00:29:12 +0200

    Add routes

--- source/config/routes.rb
+++ source/config/routes.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/' => 'home#show', as: :home
+  resources :votings, only: [] do
+    namespace :voting, path: '/' do
+      resources :partials, only: :create do
+        collection do
+          get '/raw' => :index_raw
+        end
+      end
+    end
+  end
 end
